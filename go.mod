b4ce0a40114316534c99a46fabcc252d7deb1655
Shaunak Kashyap <ycombinator@gmail.com>
2025-08-01 16:05:17 -0700

    init checkin

---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/ycombinator/go-apm-demo
+
+go 1.23.0
+
+toolchain go1.23.11
+
+require (
+	go.elastic.co/apm/module/apmhttp/v2 v2.7.1
+	go.elastic.co/apm/v2 v2.7.1
+	golang.org/x/net v0.42.0
+)
+
+require (
+	github.com/armon/go-radix v1.0.0 // indirect
+	github.com/elastic/go-sysinfo v1.15.3 // indirect
+	github.com/elastic/go-windows v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/procfs v0.17.0 // indirect
+	go.elastic.co/fastjson v1.5.1 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	howett.net/plist v1.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+
+	"go.elastic.co/apm/module/apmhttp/v2"
+	"go.elastic.co/apm/v2"
+	"golang.org/x/net/context/ctxhttp"
+)
+
+func main() {
+	printAPMEnv()
+	var tracingClient = apmhttp.WrapClient(http.DefaultClient)
+	tx := apm.DefaultTracer().StartTransaction("GET https://google.com", "request")
+	defer tx.End()
+
+	ctx := context.Background()
+	resp, err := ctxhttp.Get(ctx, tracingClient, "https://google.com")
+	if err != nil {
+		apm.CaptureError(ctx, err).Send()
+		return
+	}
+
+	tx.Result = "HTTP 2xx"
+	tx.Context.SetLabel("region", "us-east-1")
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		apm.CaptureError(ctx, err).Send()
+		return
+	}
+
+	fmt.Printf("Received %d bytes\n", len(body))
+	tx.Context.SetLabel("response_size", len(body))
+}
+
+func printAPMEnv() {
+	for _, env := range os.Environ() {
+		if strings.HasPrefix(env, "ELASTIC_APM_") {
+			fmt.Println(env)
+		}
+	}
+}
